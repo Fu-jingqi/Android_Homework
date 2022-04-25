3f29c962aa74349df467ec5ad63731e10ab6c88d
Fu jingqi <1070673506@qq.com>
2022-04-25 20:39:13 +0800

    2022/04/25

--- SDinosaurCommunity/app/src/main/java/com/henu/sdinosaurcommunity/Activity/RegisterActivity.java
+++ SDinosaurCommunity/app/src/main/java/com/henu/sdinosaurcommunity/Activity/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import com.henu.sdinosaurcommunity.R;
 public class RegisterActivity extends AppCompatActivity {
     private Button mBtnReturn = null ;
     private Button mBtnSubmit = null;
+    private TextView mRegisterReadme = null ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +34,15 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mRegisterReadme = findViewById(R.id.Register_readme) ;
+        mRegisterReadme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,DisciplineActivity.class) ;
+                startActivity(intent);
             }
         });
     }
